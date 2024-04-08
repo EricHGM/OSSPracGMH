40a71bc2b29d219c840677036132b98af8554832
EricHGM <erichwang16@dongguk.edu>
2024-04-08 16:18:45 +0900

    M

---
+++ main.py
@@ -0,0 +1,7 @@
+print('hello world')
+name = 'gmhwang'
+print(f'I\m GM Hwang!' )
+programming_language='Python'  
+print('Most used language is {0}'.format(programming_language))
+print("안녕하세요")
+##12346
